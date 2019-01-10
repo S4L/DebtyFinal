bd4454a3eaba62881ed0276a4195c2812b3c1756
Kha Tran <soltran14@hotmail.com>
2019-01-10 17:35:35 -0600

    upload project

---
+++ DebtyFinal/DebtyFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DebtyFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
