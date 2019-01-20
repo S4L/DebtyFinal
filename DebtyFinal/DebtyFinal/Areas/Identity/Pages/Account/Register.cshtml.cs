9c5b55c71da26cdc49817539dd6b1afdf82e51b0
Sol <Sol@SOL-PC>
2019-01-20 01:21:52 -0600

    ADDED:
    Register.cshtml.cs:
    FirstName, LastName, PhoneNumber, Birthday properties
    
    Register.cshtml:
    FirstName, LastName, PhoneNumber, Birthday labels and fields
    
    REGISTER, LOGIN TEST:
    Successfully register new user and login user

--- DebtyFinal/DebtyFinal/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DebtyFinal/DebtyFinal/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -41,6 +41,23 @@ namespace DebtyFinal.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name ="First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name ="Day of Birth")]
+            public DateTime Birthday { get; set; }
+
+            [Required]
+            [Phone]
+            [Display(Name = "Phone Number")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -67,7 +84,15 @@ namespace DebtyFinal.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new Person { UserName = Input.Email, Email = Input.Email };
+                var user = new Person
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Birthday = Input.Birthday,
+                    PhoneNumber = Input.PhoneNumber
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
